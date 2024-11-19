38223355ae7da763790762d74de746aa562f41e7
Maya <mayaflorentinodasilva@gmail.com>
2024-11-19 18:48:29 -0300

    Criada a função exibeMenu que é responsável por apresentar opções ao usuário e através da escolha dele retorna via api uma lista de modelos

--- src/main/java/maya/estudos/tabela_fipe/principal/Principal.java
+++ src/main/java/maya/estudos/tabela_fipe/principal/Principal.java
@@ -1,4 +1,40 @@
 package maya.estudos.tabela_fipe.principal;
 
+import maya.estudos.tabela_fipe.service.ConsumoApi;
+
+import java.util.Scanner;
+
 public class Principal {
+
+    private final ConsumoApi consumo = new ConsumoApi();
+    private final Scanner sc = new Scanner(System.in);
+    private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
+
+    public void exibeMenu() {
+        var menu = """
+                *** OPÇÕES ***
+                Carro
+                Moto
+                Caminhão
+                
+                Digite uma das opções para consulta:
+                
+                """;
+
+        System.out.println(menu);
+        var opcao = sc.nextLine();
+        String endereco;
+
+        if (opcao.toLowerCase().contains("carr")){
+            endereco = URL_BASE + "carros/marcas";
+        } else if (opcao.toLowerCase().contains("mot")) {
+            endereco = URL_BASE + "motos/marcas";
+        } else {
+            endereco = URL_BASE + "caminhoes/marcas";
+        }
+
+        var json = consumo.obterDados(endereco);
+        System.out.println(json);
+    }
+
 }
